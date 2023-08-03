4aa618c823dcc76394a17ff7d957de810ecbf2b2
Cozen <13925636+Cozen-Official@users.noreply.github.com>
2023-08-03 05:45:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='cfn', intents=intents)
+bot = commands.Bot(command_prefix='cfn!', intents=intents)
 
 
 @bot.event
