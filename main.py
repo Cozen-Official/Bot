d031d46fb3d8676dd15735cd6771cdf5315f7cf2
Cozen <13925636+Cozen-Official@users.noreply.github.com>
2023-08-05 05:02:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ server2_id = 1071613385910792262
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
+	print(f"Logged in as {bot.user}")
 
 # Command: Ping
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+	await ctx.send('pong')
 	
 #Command: Register
 #@bot.command()
@@ -28,15 +28,15 @@ async def ping(ctx):
 # Command: Broadcast	
 @bot.command(pass_context=True)
 async def broadcast(ctx, *, msg):
-    for guild in bot.guilds:
-        for channel in guild.text_channels:
-            try:
+	for guild in bot.guilds:
+		for channel in guild.text_channels:
+			try:
 				print(f"sending message to "+str(channel))
 				await channel.send(msg)
-            except Exception:
-                continue
-            else:
-                continue
+			except Exception:
+				continue
+				else:
+				continue
 
 
 bot.run(os.environ["DISCORD_TOKEN"])
