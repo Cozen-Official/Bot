4b10e46b63f48a3f8ee40d8be3c54e1896735c46
Cozen <13925636+Cozen-Official@users.noreply.github.com>
2023-08-05 06:54:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# This example requires the 'message_content' privileged intents
-
 import os
 import discord
 from discord.ext import commands
@@ -7,11 +5,7 @@ from discord.ext import commands
 # Initialize the bot
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='cfn!', intents=intents)
-
-# Server IDs
-server1_id = 1102112870583521364
-server2_id = 1071613385910792262
+bot = commands.Bot(command_prefix='/', intents=intents)
 
 # Print when Logged In
 @bot.event
@@ -23,10 +17,6 @@ async def on_ready():
 async def ping(ctx):
 	await ctx.send('pong')
 	
-#Command: Register
-#@bot.command()
-#
-
 # Command: Broadcast	
 @bot.command(pass_context=True)
 async def broadcast(ctx, *, msg):
@@ -39,7 +29,37 @@ async def broadcast(ctx, *, msg):
 				continue
 			else:
 				continue
-				
 
+@bot.command()
+async def send_embed(ctx, name: str, user_id: int):
+	embed = discord.Embed(
+		title=f'User Ban: {name}',
+		description=f'Are you sure you want to ban {name}?',
+		color=discord.Color.red()
+	)
 
+	async def ban_callback(interaction):
+		await interaction.response.send_message('Banning user...')
+		try:
+			user = await bot.fetch_user(user_id)
+			await ctx.guild.ban(user, reason='User ban requested')
+		await interaction.followup.send_message(f'Successfully banned user {name}')
+		except discord.NotFound:
+			await interaction.followup.send_message('User not found.')
+
+	action_row = discord.ui.ActionRow(
+		discord.ui.Button(label='Ban User', custom_id='ban_user', style=discord.ButtonStyle.red)
+	)
+
+	view = discord.ui.View()
+	view.add_item(action_row)
+
+	await ctx.send(embed=embed, view=view)
+
+@bot.event
+async def on_button_click(interaction):
+	if interaction.custom_id == 'ban_user':
+		await interaction.defer()
+		await ban_callback(interaction)
+		
 bot.run(os.environ["DISCORD_TOKEN"])
