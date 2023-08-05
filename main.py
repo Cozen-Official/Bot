47f4a570d2f13cf3d6641a7cd30741cbe3a8b990
Cozen <13925636+Cozen-Official@users.noreply.github.com>
2023-08-05 04:38:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -20,28 +20,10 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
-# Command: Share ban
+	
+#Command: Register
 @bot.command()
-async def shareban(ctx, user_id: int, reason: str):
-    # Get the member object for the given user ID
-    member = bot.get_user(user_id)
-
-    if not member:
-        await ctx.send("User not found.")
-    #    return
-
-    # Share the ban to both servers
-    server1 = bot.get_guild(server1_id)
-    server2 = bot.get_guild(server2_id)
-
-    if server1 and server2:
-        # Ban the user on both servers
-        await server1.ban(member, reason=reason)
-        await server2.ban(member, reason=reason)
-        await ctx.send(f"{member.mention} has been banned on both servers for: {reason}")
-    else:
-        await ctx.send("Could not find both servers.")
+async def register(ctx, 
 	
 # Command: Broadcast	
 @bot.command(pass_context=True)
